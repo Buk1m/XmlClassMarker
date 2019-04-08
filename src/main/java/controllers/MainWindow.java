a467213564eebc07fe67ac81d42f39e3c0ee1123
Bartłomiej Ciesielski <bartekc8@op.pl>
2019-04-08 21:53:24 +0200

    Add button settings for all classes, some layout changes.

--- src/main/java/controllers/MainWindow.java
+++ src/main/java/controllers/MainWindow.java
@@ -1,14 +1,22 @@
 package controllers;
 
+import java.io.File;
 import java.util.List;
 
 import controllers.view.MarkerButtonView;
 import data.MarkerDto;
 import javafx.collections.ListChangeListener.Change;
+import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 import models.MainWindowModel;
 import org.apache.commons.lang3.NotImplementedException;
 
@@ -17,6 +25,15 @@ import static org.apache.commons.collections4.CollectionUtils.isNotEmpty;
 public class MainWindow implements ModelBindable<MainWindowModel> {
     private MainWindowModel service;
     public VBox MarkerButtonBox;
+    public TextArea TextFieldArea;
+    public TextField XmlFilePathField;
+    public Button XmlFilePathButton;
+    public TextField SaveFolderField;
+    public Button SaveFolderButton;
+    public Button SaveButton;
+    public Spinner IdSpinner;
+    public Button LoadButton;
+    public Button LoadXmlFileButton;
 
     @Override
     public void setService(MainWindowModel service) {
@@ -58,5 +75,41 @@ public class MainWindow implements ModelBindable<MainWindowModel> {
         HBox.setMargin(markerView, new Insets(5.d));
 
         MarkerButtonBox.getChildren().add(hBox);
+
+        markerView.setOnAction(event -> onButtonClicked(markerView, event));
+    }
+
+    private void onButtonClicked(MarkerButtonView markerView, ActionEvent event) {
+
+    }
+
+    public void onChooseXmlFileButtonClicked() {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Wybierz plik xml");
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Pliki xml (*.xml)", "*.xml");
+        chooser.getExtensionFilters().add(extFilter);
+        File file = chooser.showOpenDialog(XmlFilePathButton.getScene().getWindow());
+
+        XmlFilePathField.setText(file.getAbsolutePath());
+    }
+
+    public void onChooseSaveFolderButtonClicked() {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Wybierz folder do zapisu");
+        File file = chooser.showDialog(SaveFolderField.getScene().getWindow());
+
+        SaveFolderField.setText(file.getAbsolutePath());
+    }
+
+    public void onLoadButtonClicked() {
+
+    }
+
+    public void onLoadXmlFileButtonClicked() {
+
+    }
+
+    public void onSaveButtonClicked() {
+
     }
 }
--- src/main/java/models/MainWindowModel.java
+++ src/main/java/models/MainWindowModel.java
@@ -25,14 +25,23 @@ public class MainWindowModel extends BaseModel {
 
     private void initMarkerButtons() {
         markers.add(new MarkerDto("CLEAR", "Wyczyść", Color.WHITE, "CTRL-Q"));
+        markers.add(new MarkerDto("czynnosc", "Czynności", Color.valueOf("#a0bd9b"), "CTRL-W"));
+        markers.add(new MarkerDto("p_ust", "Płyny", Color.valueOf("#accfff"), "CTRL-E"));
+        markers.add(new MarkerDto("narzedzie", "Narzędzia", Color.valueOf("#ed9a79"), "CTRL-R"));
+        markers.add(new MarkerDto("sub_chem", "Sub. chemiczne", Color.valueOf("#deb96a"), "CTRL-T"));
+        markers.add(new MarkerDto("narzad", "narządy", Color.valueOf("#a0eaff"), "CTRL-1"));
+        markers.add(new MarkerDto("cz_ciala", "cz. ciała", Color.valueOf("#d5adff"), "CTRL-2"));
+        markers.add(new MarkerDto("wymiar", "Wymiary", Color.valueOf("#ffadd7"), "CTRL-3"));
+        markers.add(new MarkerDto("lokalizacja", "Lokalizacje", Color.valueOf("#ffebad"), "CTRL-4"));
+        markers.add(new MarkerDto("patologia", "Patologie", Color.valueOf("#d6ffad"), "CTRL-5"));
     }
 
     void configureWindow(Stage stage, Scene scene) {
         stage.setTitle("Xml marker");
         stage.setWidth(800);
-        stage.setHeight(475);
-        stage.setMinWidth(700);
-        stage.setMinHeight(450);
+        stage.setHeight(500);
+        stage.setMinWidth(750);
+        stage.setMinHeight(500);
 
         stage.show();
     }
